6c25165986a6482fcf038bf6d39555067f68e4ba
peter lee <rheehot@gmail.com>
2021-10-02 15:06:17 +0900

    section 6-3 spring container bean study

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -1,18 +1,35 @@
 package moviebuddy;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import moviebuddy.domain.CsvMovieReader;
 import moviebuddy.domain.MovieFinder;
+import moviebuddy.domain.MovieReader;
 
 @Configuration
 public class MovieBuddyFactory {
 
 	@Bean
-	public MovieFinder movieFinder() {
+	public MovieReader movieReader() {
 
-		return new MovieFinder(new CsvMovieReader());
+		return new CsvMovieReader();
+	}
+	
+//	@Bean
+//	public MovieFinder movieFinder() {
+////		return new MovieFinder(new CsvMovieReader());
+//		return new MovieFinder(movieReader());
+//	}
+
+	@Bean
+//	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//	@Scope("prototype")
+	public MovieFinder movieFinder(MovieReader movieReader) {
+//		return new MovieFinder(new CsvMovieReader());
+		return new MovieFinder(movieReader());
 	}
 
 }
