00e10c989e5501de4be2eccc6672f53f5fd0d24c
peter lee <rheehot@gmail.com>
2021-10-02 15:16:52 +0900

    section 6-3-3 spring container bean study

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -3,6 +3,7 @@ package moviebuddy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Scope;
 
 import moviebuddy.domain.CsvMovieReader;
@@ -10,26 +11,27 @@ import moviebuddy.domain.MovieFinder;
 import moviebuddy.domain.MovieReader;
 
 @Configuration
+@Import({MovieBuddyFactory.DomainModuleConfig.class, MovieBuddyFactory.DataSourceModuleConfig.class})
 public class MovieBuddyFactory {
 
-	@Bean
-	public MovieReader movieReader() {
-
-		return new CsvMovieReader();
+	@Configuration
+	static class DomainModuleConfig {	
+		
+		@Bean
+//		@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//		@Scope("prototype")
+		public MovieFinder movieFinder(MovieReader movieReader) {
+			return new MovieFinder(movieReader);
+		}		
 	}
 	
-//	@Bean
-//	public MovieFinder movieFinder() {
-////		return new MovieFinder(new CsvMovieReader());
-//		return new MovieFinder(movieReader());
-//	}
+	@Configuration
+	static class DataSourceModuleConfig {
 
-	@Bean
-//	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-//	@Scope("prototype")
-	public MovieFinder movieFinder(MovieReader movieReader) {
-//		return new MovieFinder(new CsvMovieReader());
-		return new MovieFinder(movieReader());
+		@Bean
+		public MovieReader movieReader() {
+			return new CsvMovieReader();
+		}		
 	}
 
 }
